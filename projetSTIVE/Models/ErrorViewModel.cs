3772cdf5ad6d2848bdcf6c46b250e83f3489df39
Steven <steven.caron@viacesi.fr>
2021-10-29 10:26:01 +0200

    Création projetSTIVE

---
+++ projetSTIVE/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace projetSTIVE.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
